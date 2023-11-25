02cd98dbee7146d8b456e0344054ffbbda42574b
Alex McLain <alex@alexmclain.com>
2023-11-25 15:24:26 -0800

    Update formatter message to use ANSI.format

--- templates/new/.formatter.exs
+++ templates/new/.formatter.exs
@@ -1,9 +1,13 @@
-IO.puts """
-\u001b[33mFormatter disabled.\u001b[0m
-This repository uses a style guide.
+IO.puts IO.ANSI.format([
+  :yellow, :bright,
+  "-- The formatter is disabled --\n",
+  :reset,
+  """
+  This repository uses a style guide.
 
-See:
-  https://github.com/amclain/styleguides/blob/master/elixir/STYLEGUIDE.md
-"""
+  See:
+    https://github.com/amclain/styleguides/blob/master/elixir/STYLEGUIDE.md
+  """
+])
 
-Kernel.exit(:normal)
+exit :normal
