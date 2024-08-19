1661160e40f70974654385746dce28e224518a26
Alex McLain <alex@alexmclain.com>
2024-08-18 19:04:25 -0700

    Set up hex and docs

--- mix.exs
+++ mix.exs
@@ -7,19 +7,63 @@ defmodule RedwireTasks.MixProject do
       version: "0.1.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
+      aliases: aliases(),
+      description: description(),
+      package: package(),
       deps: deps(),
+      docs: docs(),
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
+  defp aliases do
+    [
+      "docs.show": ["docs", &open("doc/index.html", &1)],
+    ]
+  end
+
   defp deps do
-    []
+    [
+      {:ex_doc, "~> 0.34.2", only: :dev, runtime: false},
+    ]
+  end
+
+  defp description do
+    "Redwire Labs mix tasks"
+  end
+
+  defp docs do
+    [
+      extras: ["README.md", "LICENSE.txt"]
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/redwirelabs/redwire_tasks"},
+      maintainers: ["Alex McLain"],
+      files: [
+        "lib",
+        "mix.exs",
+        "LICENSE.txt",
+        "README.md",
+      ]
+    ]
+  end
+
+  # Open a file with the default application for its type.
+  defp open(file, _args) do
+    open_command =
+      System.find_executable("xdg-open") # Linux
+      || System.find_executable("open")  # Mac
+      || raise "Could not find executable 'open' or 'xdg-open'"
+
+    System.cmd(open_command, [file])
   end
 end
